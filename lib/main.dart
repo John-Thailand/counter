34f73556591ea10efb8684fc804eaf5fbdbcd4c8
John-Thailand <hamoban69@gmail.com>
2022-06-09 21:45:03 +0900

    ProviderObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
+// Riverpod を使用した Logger 付きのカウンターアプリの例
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class Logger extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    print('''
+{
+  "provider": "${provider.name ?? provider.runtimeType}",
+  "newValue": "$newValue"
+}''');
+  }
+}
+
 void main() {
   runApp(
-    // Adding ProviderScope enables Riverpod for the entire project
-    const ProviderScope(child: MyApp()),
+    // ProviderScope を置くことで Riverpod が有効になる
+    // Logger インスタンスを observers のリストに追加する
+    ProviderScope(observers: [Logger()], child: const MyApp()),
   );
 }
 
@@ -13,34 +32,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
-    );
+    return MaterialApp(home: Home());
   }
 }
 
-/// Providers are declared globally and specify how to create a state
-final counterProvider = StateProvider((ref) => 0);
+final counterProvider = StateProvider((ref) => 0, name: 'counter');
 
 class Home extends ConsumerWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Counter example')),
       body: Center(
-        // Consumer is a widget that allows you reading providers.
-        child: Consumer(builder: (context, ref, _) {
-          // 2. ref.watchはプロバイダーの状態の更新を監視する。そして、新しい状態を表示している。
-          final count = ref.watch(counterProvider.state).state;
-          return Text('$count');
-        }),
+        child: Text('$count'),
       ),
       floatingActionButton: FloatingActionButton(
-        // The read method is a utility to read a provider without listening to it
-        // 1. counterProviderの状態を読む。その状態をインクリメントする。
-        onPressed: () => ref.read(counterProvider.state).state++,
+        onPressed: () => ref.read(counterProvider.notifier).state++,
         child: const Icon(Icons.add),
       ),
     );
